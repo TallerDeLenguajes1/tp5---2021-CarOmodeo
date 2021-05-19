ca8b8126b6a504ef039c32bcf1f4c9a14dc2cdc8
Carolina Perez Omodeo <caro.omodeo@gmail.com>
2021-05-19 13:23:37 -0300

    Ejercicion 1 y 2 completos

---
+++ Tp5_PrimerProyectoConsola/Ejercicio2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace Tp5Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta;
+            do
+            {
+                float num1, num2, resultado;
+                int operacion;
+                string tipoOperacion;
+
+                do
+                {
+                    Console.WriteLine("Ingrese el codigo de la operacion que desea realizar (1.Suma, 2.Resta, 3.Multiplicacion, 4.Division):");
+                    operacion = Convert.ToInt32(Console.ReadLine());
+                } while (operacion != 1 && operacion != 2 && operacion != 3 && operacion != 4);
+
+                Console.WriteLine("Ingrese un numero");
+                num1 = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese otro numero");
+                num2 = float.Parse(Console.ReadLine());
+
+                switch (operacion)
+                {
+                    case 1:
+                        resultado = num1 + num2;
+                        tipoOperacion = "Suma";
+                        break;
+                    case 2:
+                        resultado = num1 - num2;
+                        tipoOperacion = "Resta";
+                        break;
+                    case 3:
+                        resultado = num1 * num2;
+                        tipoOperacion = "Multiplicacion";
+                        break;
+                    default:
+                        resultado = num1 / num2;
+                        tipoOperacion = "Division";
+                        break;
+
+                }
+
+                Console.WriteLine("La " + tipoOperacion + " es igual a " + Math.Round(resultado,2));
+
+
+                Console.WriteLine("Desea realizar otra operacion? (Si/NO)");
+                respuesta = Console.ReadLine().ToLower();
+            } while (respuesta.Equals("si") || respuesta.Equals("s"));
+        }
+    }
+}
---
+++ Tp5_PrimerProyectoConsola/Tp5_PrimerProyectoConsola/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tp5Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b = 0;
+            Console.WriteLine("Ingrese un numero entero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if(a > 0)
+            {
+                while(a > 0)
+                {
+                    b = b * 10 + (a % 10);
+                    a /= 10;
+                }
+                Console.WriteLine("El num invertido es " + b);
+            }
+            else
+            {
+                Console.WriteLine("No se puede realizar la invercion del numero");
+            }
+        }
+    }
+}
